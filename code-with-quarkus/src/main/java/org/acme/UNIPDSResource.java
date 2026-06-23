c8cb79d5cad55a0b87a370e9583f5c6ca17a9f2d
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-06-22 22:27:04 -0300

    set app path to /api

--- code-with-quarkus/src/main/java/org/acme/UNIPDSResource.java
+++ code-with-quarkus/src/main/java/org/acme/UNIPDSResource.java
@@ -1,7 +1,37 @@
 package org.acme;
 
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/unipds")
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
 public class UNIPDSResource {
+    private int i = 0;
+
+    @GET
+    public int getI(){
+        return i;
+    }
+
+    @GET
+    @Path("/time")
+    public String getTime(){
+        return "The time is " + System.currentTimeMillis();
+    }
+
+    @POST
+    public void addI(){
+        this.i++;
+    }
+
+    @PUT
+    public void setI(int i){
+        this.i = i;
+    }
+
+    @DELETE
+    public void deleteI(){
+        this.i--;
+    }
 }
