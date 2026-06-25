476671b4111f774db435002930726dd6a2044cc9
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-06-25 14:12:04 -0300

    adds timeout and fallback

--- code-with-quarkus/src/main/java/org/acme/StarWarsService.java
+++ code-with-quarkus/src/main/java/org/acme/StarWarsService.java
@@ -4,12 +4,22 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@RegisterRestClient(baseUri = "https://swapi.info/api/")
+@RegisterRestClient(baseUri = "https://swapi.info/asapi/")
 public interface StarWarsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("starships")
+    @Timeout(value = 3000L)
+    @Fallback(
+            fallbackMethod="getStarshipsFallback"
+    )
     String getStarships();
+
+    default String getStarshipsFallback(){
+        return "fallback";
+    }
 }
