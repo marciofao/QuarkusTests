c69513f538c2618f56fb4eba46fe655f8bf907ad
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-06-25 17:14:14 -0300

    implement health liveness and readyness

---
+++ code-with-quarkus/src/main/java/org/acme/ReadinessCheck.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Readiness
+public class ReadinessCheck implements HealthCheck {
+
+    @RestClient
+    StarWarsService starWarsService;
+
+    @Override
+    public HealthCheckResponse call(){
+        if(starWarsService.getStarships().contains(StarWarsService.MSG_ERROR)){
+            return HealthCheckResponse.down("Error in StarWarsService")
+        }else{
+            return HealthCheckResponse.up("OK")
+        }
+    }
+}
