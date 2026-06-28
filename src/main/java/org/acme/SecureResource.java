157a1199bd2cc111939337c5e8c550eaa85db58a
Marcio Fao <marcio.lopes.fao@gmail.com>
2026-06-28 14:36:51 -0300

    Add role annotation to secure

--- src/main/java/org/acme/SecureResource.java
+++ src/main/java/org/acme/SecureResource.java
@@ -1,8 +1,8 @@
 package org.acme;
 
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.annotation.security.PermitAll;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import org.eclipse.microprofile.jwt.Claim;
@@ -12,24 +12,23 @@ import org.eclipse.microprofile.jwt.Claims;
 @RequestScoped
 public class SecureResource {
 
-    @Inject
     @Claim(standard = Claims.preferred_username)
     private String username;
 
     @GET
     @Path("claim")
-    @PermitAll
+    @RolesAllowed( "Subscriber")
     public String getClaim() {
         return username;
     }
 }
 
 /*
-* Usage in PS:
-*
-* $token = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/eldermoraes/unipds/main/jwt-token/quarkus.jwt.token"
-*
-* Invoke-WebRequest -Uri "http://localhost:8080/api/secure/claim" `
->>     -Headers @{ Authorization = "Bearer $token" } `
->>     -Method Get
+ Usage in PS:
+
+ $token = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/eldermoraes/unipds/main/jwt-token/quarkus.jwt.token"
+
+ Invoke-WebRequest -Uri "http://localhost:8080/api/secure/claim" `
+    -Headers @{ Authorization = "Bearer $token" } `
+    -Method Get
 * */
